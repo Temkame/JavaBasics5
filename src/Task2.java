5fd6992718d903fb07242546f1c3bb8cc3f7f2ea
temkame <temkame@yandex.ru>
2022-03-25 18:22:46 +0300

    Commit

---
+++ src/Task2.java
@@ -0,0 +1,15 @@
+/*
+0 чисел наоборот
+
+Вывести на экран числа от 10 до 1, используя цикл while.
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        int a = 10;
+
+        while (a >= 1) {
+            System.out.println(a);
+            a--;
+        }
+    }
+}
