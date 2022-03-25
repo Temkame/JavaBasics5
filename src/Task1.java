2faa6173179acb93d6f4eb9db6bed6451330a06c
temkame <temkame@yandex.ru>
2022-03-25 18:17:44 +0300

    Initial commit

---
+++ src/Task1.java
@@ -0,0 +1,15 @@
+/*
+10 чисел
+
+Вывести на экран числа от 1 до 10, используя цикл while.
+ */
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 0;
+
+        while (a < 10) {
+            a++;
+            System.out.println(a);
+        }
+    }
+}
